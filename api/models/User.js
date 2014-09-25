abc0906490833e4606293e2ce96958c82f8b2c3f
Mike McNeil <mike@balderdash.co>
2014-09-25 16:31:46 -0400

    Add placekitten

--- api/models/User.js
+++ api/models/User.js
@@ -21,6 +21,8 @@ module.exports = {
     name: {
       type: 'string'
     }
-  }
+  },
+
+  connection: 'myLocalMongo'
 };
 
--- config/connections.js
+++ config/connections.js
@@ -49,7 +49,7 @@ module.exports.connections = {
   // Run:
   // npm install sails-mongo
   //
-  someMongodbServer: {
+  myLocalMongo: {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
